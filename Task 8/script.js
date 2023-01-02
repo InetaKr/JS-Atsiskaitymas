159d3f88c5c297669ea0026904041c288e184118
InetaKr <ineta.kriunaite@outlook.com>
2023-01-02 11:09:01 +0200

    8 is done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,37 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    constructor() {}
+    
+    sum(a, b) {
+      return a + b;
+    }
+    
+    subtraction(a, b) {
+      return a - b;
+    }
+    
+    multiplication(a, b) {
+      return a * b;
+    }
+    
+    division(a, b) {
+      return a / b;
+    }
+  }
+
+const calc = new Calculator();
+
+let result = calc.sum(1, 2);
+console.log(result);
+
+result = calc.subtraction(5, 3);
+console.log(result);
+
+result = calc.multiplication(2, 3);
+console.log(result); 
+
+result = calc.division(10, 5);
+console.log(result);
