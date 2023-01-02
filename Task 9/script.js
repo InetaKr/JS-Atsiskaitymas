96c599351cf685a65c6d703e148d6813f91f02e9
InetaKr <ineta.kriunaite@outlook.com>
2023-01-02 11:42:19 +0200

    10 done

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,20 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = budget;
+    }
+  
+    wasExpensive() {
+      return this.budget > 100000000;
+    }
+  }
+
+const movie1 = new Movie('Avengers: Endgame', 'Anthony Russo', 356000000);
+console.log(movie1.wasExpensive()); // true
+
+const movie2 = new Movie('The Blair Witch Project', 'Daniel Myrick', 60000);
+console.log(movie2.wasExpensive()); // false
